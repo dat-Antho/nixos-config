a7a3534edb6f8bf269305c5f8c95adde006217be
Anthony <16465475+dat-Antho@users.noreply.github.com>
2024-11-16 14:18:41 +0100

    tweak subsituters priority

--- configuration.nix
+++ configuration.nix
@@ -13,9 +13,9 @@
   nix = {
     settings = {
       substituters = [
-        "https://nix-community.cachix.org"
-        "https://cache.nixos.org/"
-        "https://datantho-nixos.cachix.org"
+        "https://nix-community.cachix.org?priority=3"
+        "https://cache.nixos.org?priority=2"
+        "https://datantho-nixos.cachix.org?priority=1"
       ];
       trusted-public-keys = [
         "datantho-nixos.cachix.org-1:7mXkZZm1vhW5N0xNuMaYQh/lipZKopDEHXKpcsiDWt8="
