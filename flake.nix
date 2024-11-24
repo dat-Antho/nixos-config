c7eebe7b3a9a5d1623f2bf9d0b75b85d15ad15d8
Anthony <16465475+dat-Antho@users.noreply.github.com>
2024-11-24 07:25:33 +0100

    added home-manager

--- flake.nix
+++ flake.nix
@@ -1,12 +1,23 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
   outputs = { self, nixpkgs }: {
     nixosConfigurations = {
       nixos = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
-        modules = [ ./configuration.nix ];
+        modules = [
+          ./configuration.nix
+        home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.anthony = import ./home.nix;
+          }
+        ];
+        
       };
     };
   };
