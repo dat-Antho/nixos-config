998d1b5dea9717252a4015dbc7658975f7ed1fd5
Anthony <dat-Antho@users.noreply.github.com>
2024-12-03 09:23:28 +0100

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -128,8 +128,10 @@
   environment.sessionVariables = {
     STEAM_EXTRA_COMPAT_TOOLS_PATHS = "\${HOME}/.steam/root/compatibilitytools.d";
   };
-  fonts.packages = with pkgs; [
-    (nerd-fonts.override { fonts = [ "JetBrainsMono" ]; })
+  fonts = with pkgs; [
+    # nerdfonts             # fonts with icons
+    # now nerdfonts are separated in individual packages :
+    nerd-fonts.jetbrains-mono
   ];
   services.syncthing = {
     enable = true;
