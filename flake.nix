9c35663a8d8b9d07828f148f82e3c9d253c82c93
Anthony <16465475+dat-Antho@users.noreply.github.com>
2025-01-12 19:08:53 +0100

    rewrote homemanager inputs

--- flake.nix
+++ flake.nix
@@ -1,8 +1,11 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    home-manager.url = "github:nix-community/home-manager";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+
+    };
   };
   outputs =
     {
