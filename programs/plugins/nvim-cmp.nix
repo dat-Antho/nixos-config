7e7a49d92f0120673fc93995c661ff39e86f71f6
Anthony <16465475+dat-Antho@users.noreply.github.com>
2025-01-13 20:54:22 +0100

    added nvim signature help

--- programs/plugins/nvim-cmp.nix
+++ programs/plugins/nvim-cmp.nix
@@ -42,12 +42,11 @@
           # Accept ([y]es) the completion.
           #  This will auto-import if your LSP supports it.
           #  This will expand snippets if the LSP sent a snippet.
-          "<C-y>" = "cmp.mapping.confirm { select = true }";
           # If you prefer more traditional completion keymaps,
           # you can uncomment the following lines.
-          # "<CR>" = "cmp.mapping.confirm { select = true }";
-          # "<Tab>" = "cmp.mapping.select_next_item()";
-          # "<S-Tab>" = "cmp.mapping.select_prev_item()";
+          "<CR>" = "cmp.mapping.confirm { select = true }";
+          "<Tab>" = "cmp.mapping.select_next_item()";
+          "<S-Tab>" = "cmp.mapping.select_prev_item()";
 
           # Manually trigger a completion from nvim-cmp.
           #  Generally you don't need this, because nvim-cmp will display
@@ -103,6 +102,9 @@
           # https://nix-community.github.io/nixvim/plugins/cmp-path.html
           {
             name = "path";
+          }
+          {
+            name = "nvim_lsp_signature_help";
           }
         ];
       };
