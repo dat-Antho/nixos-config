68cacd3ebb236899b166cdc5ff46dae8544698e9
Anthony <dat-Antho@users.noreply.github.com>
2025-01-23 20:42:43 +0100

    Update configuration.nix to use latest linux kernel

--- configuration.nix
+++ configuration.nix
@@ -38,6 +38,8 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.grub.device = "/dev/nvme0n1p1";
+  # Use latest kernel
+  boot.kernelPackages = pkgs.linuxPackages_latest;
   # Enable OpenGL
   hardware.graphics.enable = true;
   hardware.keyboard.qmk.enable = true;
