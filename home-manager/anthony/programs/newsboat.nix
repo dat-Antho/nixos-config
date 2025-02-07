321696a778afb706304c58d6eec77c9ef9a67e0f
Anthony <dat-Antho@users.noreply.github.com>
2025-02-07 19:24:14 +0100

    Update newsboat.nix

--- home-manager/anthony/programs/newsboat.nix
+++ home-manager/anthony/programs/newsboat.nix
@@ -8,6 +8,7 @@
       { url = "https://nixos.org/blog/announcements-rss.xml"; }
       { url = "https://www.reddit.com/r/linux_gaming/top/.rss?sort=top&t=week"; }
       { url = "https://www.gamingonlinux.com/article_rss.php"; }
+      { url= "https://www.youtube.com/feeds/videos.xml?channel_id=UCUkyyRJdVdXFmBuxPLFYmPw"; }
     ];
   };
 }
