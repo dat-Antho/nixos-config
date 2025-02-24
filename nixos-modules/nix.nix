4798834dbea83ff45a1412120c62784d49fb9aea
Anthony <dat-Antho@users.noreply.github.com>
2025-02-24 15:19:08 +0100

    Update nix.nix

--- nixos-modules/nix.nix
+++ nixos-modules/nix.nix
@@ -6,11 +6,9 @@
       substituters = [
         "https://datantho-nixos.cachix.org?priority=1"
         "https://cache.nixos.org?priority=2"
-        "https://nix-community.cachix.org?priority=3"
-      ];
+];
       trusted-public-keys = [
         "datantho-nixos.cachix.org-1:7mXkZZm1vhW5N0xNuMaYQh/lipZKopDEHXKpcsiDWt8="
-        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       ];
     };
   };
