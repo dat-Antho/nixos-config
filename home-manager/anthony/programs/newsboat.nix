858ae61c028cb7931b35093722d89088efa26520
Anthony <16465475+dat-Antho@users.noreply.github.com>
2025-03-31 19:44:28 +0200

    added tech rss in newsboat

--- home-manager/anthony/programs/newsboat.nix
+++ home-manager/anthony/programs/newsboat.nix
@@ -9,6 +9,7 @@
       { url = "https://www.reddit.com/r/linux_gaming/top/.rss?sort=top&t=week"; }
       { url = "https://www.gamingonlinux.com/article_rss.php"; }
       { url = "https://www.youtube.com/feeds/videos.xml?channel_id=UCUkyyRJdVdXFmBuxPLFYmPw"; }
+      { url = "https://tldr.tech/api/rss/tech"; }
     ];
   };
 }
