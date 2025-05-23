70e387653a1652beb3399a9157005f9f03f76a01
Anthony <dat-Antho@users.noreply.github.com>
2025-05-24 00:01:49 +0200

    Update zsh.nix

--- home-manager/anthony/programs/zsh.nix
+++ home-manager/anthony/programs/zsh.nix
@@ -10,8 +10,8 @@
       ll = "ls -l";
       update = "sudo nixos-rebuild switch";
       kde-fix-icons = "sed -i 's/file:\/\/\/nix\/store\/[^\/]*\/share\/applications\//applications:/gi' ~/.config/plasma-org.kde.plasma.desktop-appletsrc && systemctl restart --user plasma-plasmashell";
-      nrb = "nh os boot .";
-      nrs = "nh os switch .";
+      nrb = "nh os boot . -- --accept-flake-config ";
+      nrs = "nh os switch . -- --accept-flake-config";
       ghfb = "gh workflow run flake-bump";
       ghfbl = "gh run list --workflow flake-bump.yml";
     };
