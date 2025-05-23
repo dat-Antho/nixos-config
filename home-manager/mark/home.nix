5c9a2a8cf08f850293f0b11bc0adc627da959043
Anthony <16465475+dat-Antho@users.noreply.github.com>
2025-05-23 21:17:12 +0200

    removed uncessary comments

--- home-manager/mark/home.nix
+++ home-manager/mark/home.nix
@@ -1,14 +1,9 @@
 { config, pkgs, ... }:
 
 {
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
   home.username = "anthony";
   home.homeDirectory = "/home/anthony";
 
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
   #
   # You should not change this value, even if you update Home Manager. If you do
   # want to update the value, then make sure to first check the Home Manager
@@ -17,8 +12,6 @@
   imports = [
 	../anthony/programs/nixvim.nix
   ];
-  # The home.packages option allows you to install Nix packages into your
-  # environment.
   home.packages = [
     pkgs.git
   ];
