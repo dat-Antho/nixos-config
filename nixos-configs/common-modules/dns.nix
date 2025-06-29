7d7a1d639b67c64522de36613432d7571327dac9
Anthony <16465475+dat-Antho@users.noreply.github.com>
2025-06-29 21:00:27 +0200

    updated dns to use dnscrypt

--- nixos-configs/common-modules/dns.nix
+++ nixos-configs/common-modules/dns.nix
@@ -1,12 +1,32 @@
 {
   config,
   pkgs,
+  lib,
   ...
-}: {
-  networking.nameservers = [
-    "1.1.1.1"
-    "1.0.0.1"
-    "2606:4700:4700::1111"
-    "2606:4700:4700::1001"
-  ];
+}:
+{
+
+  networking.useHostResolvConf = false;
+
+  # Forward all DNS to dnscrypt-proxy (local stub resolver)
+  networking.nameservers = [ "127.0.0.1" ];
+
+  services.dnscrypt-proxy2 = {
+    enable = true;
+
+    settings = {
+      ipv4_servers = true;
+      require_dnssec = true;
+      require_nolog = true;
+      require_nofilter = true;
+      doh_servers = true;
+      dnscrypt_servers = true;
+
+      # You can choose resolvers from https://dnscrypt.info/public-servers
+      server_names = [
+        "cloudflare"
+        "quad9-doh"
+      ];
+    };
+  };
 }
