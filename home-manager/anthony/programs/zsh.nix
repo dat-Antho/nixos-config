bf31766f8bec59752ad8bfaffae68ba9781e07aa
Anthony <dat-Antho@users.noreply.github.com>
2025-09-06 18:21:28 +0200

    Update zsh.nix

--- home-manager/anthony/programs/zsh.nix
+++ home-manager/anthony/programs/zsh.nix
@@ -9,7 +9,6 @@
       extraAliases = {
         kde-fix-icons = "sed -i 's/file:\/\/\/nix\/store\/[^\/]*\/share\/applications\//applications:/gi' ~/.config/plasma-org.kde.plasma.desktop-appletsrc && systemctl restart --user plasma-plasmashell";
         ghfb = "gh workflow run flake-bump";
-        ssh = "kitty +kitten ssh";
         ghfbl = "gh run list --workflow flake-bump.yml";
       };
     })
