515cc1b56619f48b91d54cd5b757d5874e278c4a
Anthony <dat-Antho@users.noreply.github.com>
2025-09-14 18:48:30 +0200

    Update home.nix

--- home-manager/revan/home.nix
+++ home-manager/revan/home.nix
@@ -20,7 +20,7 @@
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = with pkgs; [
-    python311Full
+    python311
     python311Packages.pip
     python311Packages.virtualenv
     python311Packages.mypy
