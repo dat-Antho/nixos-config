bfb63bb2d71748130174060ec8fb87794aeeae89
Anthony <16465475+dat-Antho@users.noreply.github.com>
2025-11-12 19:11:42 +0100

    update syncthing config to have better directory handling + migration script

--- nixos-configs/common-modules/syncthing.nix
+++ nixos-configs/common-modules/syncthing.nix
@@ -7,6 +7,18 @@
     enable = true;
     openDefaultPorts = true;
     user = "anthony";
+    dataDir = "/home/anthony/.config/syncthing-data";
   };
   systemd.services.syncthing.environment.STNODEFAULTFOLDER = "true"; # Don't create default ~/Sync folder from syncthing
+  # migration script 
+  system.activationScripts.syncthing-migrate = ''
+    old="/var/lib/syncthing"
+    new="/home/anthony/.config/syncthing-data"
+    if [ -d "$old" ] && [ ! -d "$new" ]; then
+      echo "Migrating Syncthing data from $old to $new..."
+      mkdir -p "$new"
+      cp -a "$old/." "$new/"
+      chown -R anthony:users "$new"
+    fi
+  '';
 }
