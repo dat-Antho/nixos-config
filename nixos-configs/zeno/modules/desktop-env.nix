df0d0af22e6b25fe1a26c66f8edeb84df044b386
Anthony <dat-Antho@users.noreply.github.com>
2025-11-25 08:48:39 +0100

    Update desktop-env.nix

--- nixos-configs/zeno/modules/desktop-env.nix
+++ nixos-configs/zeno/modules/desktop-env.nix
@@ -5,5 +5,6 @@
   # Enable the X11 windowing system.
   services.xserver.enable = true;
   services.displayManager.sddm.enable = true;
+  services.displayManager.sddm.wayland.enable = true;
   services.desktopManager.plasma6.enable = false;
 }
