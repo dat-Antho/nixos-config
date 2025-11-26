9439d21fd150f3a846b33f5ba9f2ec24dbfb0864
Anthony <16465475+dat-Antho@users.noreply.github.com>
2025-11-26 21:32:59 +0100

    moved foot to command nixos home-manager configs

--- flake.nix
+++ flake.nix
@@ -80,6 +80,7 @@
 
                 ./home-manager/common/programs/nixvim.nix
                 ./home-manager/common/programs/hyprland.nix
+                ./home-manager/common/programs/foot.nix
               ];
               home-manager.users.${user} = import ./home-manager/${home-manager-directory}/home.nix;
               nix.settings.trusted-users = [
