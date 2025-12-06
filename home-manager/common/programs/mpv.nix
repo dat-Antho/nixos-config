6b76bb1f5a14f29f3dc1b52c29539d59eea0dcf0
Anthyko <16465475+Anthyko@users.noreply.github.com>
2025-12-06 19:57:49 +0100

    update mpv config

--- home-manager/common/programs/mpv.nix
+++ home-manager/common/programs/mpv.nix
@@ -4,6 +4,18 @@
 }: {
   programs.mpv = {
     enable = true;
+      package = (
+    pkgs.mpv-unwrapped.wrapper {
+      scripts = with pkgs.mpvScripts; [
+        uosc
+        sponsorblock
+      ];
+
+      mpv = pkgs.mpv-unwrapped.override {
+        waylandSupport = true;
+      };
+    }
+  );
     config = {
       vo = "gpu";
       gpu-api = "opengl";
