23e17b4df18a76e368d012e0549d76a6a1665719
Anthyko <16465475+Anthyko@users.noreply.github.com>
2026-01-01 11:48:43 +0100

    added aliases

--- home-manager/anthony/programs/zsh.nix
+++ home-manager/anthony/programs/zsh.nix
@@ -10,6 +10,8 @@
         kde-fix-icons = "sed -i 's/file:\/\/\/nix\/store\/[^\/]*\/share\/applications\//applications:/gi' ~/.config/plasma-org.kde.plasma.desktop-appletsrc && systemctl restart --user plasma-plasmashell";
         ghfb = "gh workflow run flake-bump";
         ghfbl = "gh run list --workflow flake-bump.yml";
+        ghb = "gh workflow run Builds-configs";
+        ghbl = "gh run list --workflow build.yml";
       };
     })
   ];
