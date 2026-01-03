2a941338d099d8fdd43ac5dd8fe88d698d12304c
Anthyko <16465475+Anthyko@users.noreply.github.com>
2026-01-03 22:13:53 +0100

    corrected typo

--- home-manager/anthony/programs/zsh.nix
+++ home-manager/anthony/programs/zsh.nix
@@ -10,7 +10,7 @@
         kde-fix-icons = "sed -i 's/file:\/\/\/nix\/store\/[^\/]*\/share\/applications\//applications:/gi' ~/.config/plasma-org.kde.plasma.desktop-appletsrc && systemctl restart --user plasma-plasmashell";
         ghfb = "gh workflow run flake-bump";
         ghfbl = "gh run list --workflow flake-bump.yml";
-        ghb = "gh workflow run Builds-configs";
+        ghb = "gh workflow run Build-configs";
         ghbl = "gh run list --workflow build.yml";
       };
     })
