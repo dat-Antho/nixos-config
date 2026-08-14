5c03521293aa8a9e699439e2e8e2fc257f6d6916
Anthyko <16465475+Anthyko@users.noreply.github.com>
2026-08-14 20:33:15 +0200

    Edited formatter module to not have devShell

--- modules/formatter.nix
+++ modules/formatter.nix
@@ -3,25 +3,17 @@
   imports = [
     inputs.git-hooks-nix.flakeModule
   ];
-  perSystem =
-    { config, pkgs, ... }:
-    {
-      pre-commit.settings.hooks = {
-        nixfmt.enable = true;
-        deadnix.enable = true;
-        # statix.enable = true;
+  perSystem = {
+    pre-commit.settings.hooks = {
+      nixfmt.enable = true;
+      deadnix.enable = true;
+      # statix.enable = true;
 
-        check-merge-conflicts.enable = true;
-        detect-private-keys.enable = true;
-        end-of-file-fixer.enable = true;
-        trim-trailing-whitespace.enable = true;
-      };
-
-      devShells.default = pkgs.mkShell {
-        shellHook = ''
-          ${config.pre-commit.installationScript}
-          exit
-        '';
-      };
+      check-merge-conflicts.enable = true;
+      detect-private-keys.enable = true;
+      end-of-file-fixer.enable = true;
+      trim-trailing-whitespace.enable = true;
     };
+
+  };
 }
