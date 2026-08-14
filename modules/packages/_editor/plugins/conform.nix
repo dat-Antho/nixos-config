79942ba15300d4b5aa8ba2df9ea9d6fab0e64aa8
Anthyko <16465475+Anthyko@users.noreply.github.com>
2026-08-14 20:32:45 +0200

    updated format for nix files

--- modules/packages/_editor/plugins/conform.nix
+++ modules/packages/_editor/plugins/conform.nix
@@ -3,10 +3,13 @@
   # Dependencies
   #
   # https://nix-community.github.io/nixvim/NeovimOptions/index.html?highlight=extraplugins#extrapackages
+
   extraPackages = with pkgs; [
+
     # Used to format Lua code
     stylua
     black
+    nixfmt
   ];
 
   # Autoformat
@@ -19,9 +22,11 @@
         lua = [ "stylua" ];
         # Conform can also run multiple formatters sequentially
         python = [ "black" ];
+        nix = [ "nixfmt" ];
 
         #
         # You can use a sublist to tell conform to run *until* a formatter
+
         # is found
         # javascript = [ [ "prettierd" "prettier" ] ];
       };
