feb9dbfec0d7dd0ab9c1ab98b7e66c3b886051ed
Anthyko <16465475+Anthyko@users.noreply.github.com>
2026-08-17 18:07:15 +0200

    enabled format on save

--- modules/packages/_editor/plugins/conform.nix
+++ modules/packages/_editor/plugins/conform.nix
@@ -1,34 +1,27 @@
 { pkgs, ... }:
 {
+
   # Dependencies
   #
   # https://nix-community.github.io/nixvim/NeovimOptions/index.html?highlight=extraplugins#extrapackages
 
   extraPackages = with pkgs; [
-
-    # Used to format Lua code
-    stylua
     black
     nixfmt
   ];
 
-  # Autoformat
   # https://nix-community.github.io/nixvim/plugins/conform-nvim.html
   plugins.conform-nvim = {
     enable = true;
     settings = {
-      notify_on_error = false;
+      notify_on_error = true;
       formatters_by_ft = {
-        lua = [ "stylua" ];
-        # Conform can also run multiple formatters sequentially
         python = [ "black" ];
         nix = [ "nixfmt" ];
-
-        #
-        # You can use a sublist to tell conform to run *until* a formatter
-
-        # is found
-        # javascript = [ [ "prettierd" "prettier" ] ];
+      };
+      format_on_save = {
+        timeout_ms = 500;
+        lsp_fallback = false;
       };
     };
   };
@@ -40,7 +33,7 @@
       key = "<leader>f";
       action.__raw = ''
         function()
-          require('conform').format { async = true, lsp_fallback = true }
+          require('conform').format { async = true, lsp_fallback = false }
         end
       '';
       options = {
