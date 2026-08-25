0e43234c10ab9a7fd6bd02905d7bdb93dc8b345d
Anthyko <16465475+Anthyko@users.noreply.github.com>
2026-08-25 20:11:37 +0200

    small cleaning in mknixos

--- modules/mk.nix
+++ modules/mk.nix
@@ -27,10 +27,13 @@
       modules = [
         inputs.home-manager.nixosModules.home-manager
         {
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
-          home-manager.backupFileExtension = "";
-          home-manager.extraSpecialArgs = { inherit inputs; };
+          home-manager = {
+
+            useGlobalPkgs = true;
+            useUserPackages = true;
+            backupFileExtension = "";
+            extraSpecialArgs = { inherit inputs; };
+          };
         }
       ]
       ++ modules;
