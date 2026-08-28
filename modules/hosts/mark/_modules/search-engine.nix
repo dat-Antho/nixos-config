ebda5bda4f6844771b7cad669bc6709d58a8f816
Anthyko <16465475+Anthyko@users.noreply.github.com>
2026-08-29 00:28:36 +0200

    refactored search engine access
    Nginx is no longer needed for this

--- modules/hosts/mark/_modules/search-engine.nix
+++ modules/hosts/mark/_modules/search-engine.nix
@@ -1,13 +1,7 @@
 {
   config,
-  pkgs,
   ...
 }:
-let
-  tailscale_domain = "mark.tailf9979f.ts.net";
-  tailscale_ip = "100.118.197.94";
-  tailscale_cert_directory = "/var/lib/tailscale-auto-certs";
-in
 {
 
   sops.secrets."searx/env" = {
@@ -47,66 +41,20 @@ in
       ];
 
     };
-    # setup reverse proxy
   };
-  services.nginx = {
-    recommendedProxySettings = true;
-    recommendedTlsSettings = true;
-
-    virtualHosts."${tailscale_domain}" = {
-      listen = [
-        {
-          addr = tailscale_ip;
-          port = 443;
-          ssl = true;
-        }
-      ];
-
-      onlySSL = true;
-      sslCertificate = "${tailscale_cert_directory}/${tailscale_domain}.crt";
-      sslCertificateKey = "${tailscale_cert_directory}/${tailscale_domain}.key";
-
-      locations."/" = {
-        proxyPass = "http://127.0.0.1:8081";
-        proxyWebsockets = true;
-      };
-    };
-  };
-
-  # setup permission to allow nginx to read certificates
-  systemd.services.tailscale-cert-searx = {
-
-    description = "Fetch/renew Tailscale TLS cert for nginx";
+  # I could not make the services.tailscale module serve this.
+  # I'll use this for now
+  systemd.services.tailscale-search = {
+    description = "Tailscale Serve search";
+    after = [ "tailscaled.service" ];
+    wants = [ "tailscaled.service" ];
 
     wantedBy = [ "multi-user.target" ];
 
     serviceConfig = {
       Type = "oneshot";
-      UMask = "0077";
-    };
-    script = ''
-      install -d -m 0750 -o nginx -g nginx ${tailscale_cert_directory}
-
-      ${pkgs.tailscale}/bin/tailscale cert \
-        --cert-file ${tailscale_cert_directory}/${tailscale_domain}.crt \
-        --key-file  ${tailscale_cert_directory}/${tailscale_domain}.key \
-        ${tailscale_domain}
-
-      chown nginx:nginx ${tailscale_cert_directory}/${tailscale_domain}.crt ${tailscale_cert_directory}/${tailscale_domain}.key
-      chmod 0640 ${tailscale_cert_directory}/${tailscale_domain}.crt
-      chmod 0640 ${tailscale_cert_directory}/${tailscale_domain}.key
-
-      ${pkgs.systemd}/bin/systemctl reload nginx
-    '';
-  };
-
-  ## to refresh the cert
-  systemd.timers.tailscale-cert-searx = {
-    wantedBy = [ "timers.target" ];
-    timerConfig = {
-      OnCalendar = "daily";
-      RandomizedDelaySec = "2h";
-      Persistent = true;
+      RemainAfterExit = true;
+      ExecStart = "${config.services.tailscale.package}/bin/tailscale serve --service=svc:search http://127.0.0.1:8081";
     };
   };
 }
